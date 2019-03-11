d0bf3f113d818e876ab417286f2a09e27202e2fa
hristijan112 <hristijan0stojkovski@gmail.com>
2019-03-11 22:32:45 +0100

    Translate Activity finished.

--- app/src/main/java/com/example/domasna_pmp2/MainActivity.java
+++ app/src/main/java/com/example/domasna_pmp2/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnInsert = (Button) findViewById(R.id.button4);
         Button btnDelete = (Button) findViewById(R.id.button5);
+        Button btnTranslate = findViewById(R.id.button2);
+        final EditText editTxt = (EditText) findViewById(R.id.editText);
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +36,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final TextView txtView = (TextView) findViewById(R.id.textView);
 
+        btnTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Scanner scanner = new Scanner(getResources().openRawResource(R.raw.text));
+                String txtToCompare = editTxt.getText().toString();
+                boolean isFound = false;
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] delovi = line.split("/t");
+                    if (delovi[0].equals(txtToCompare)) {
+                        txtView.setText(delovi[1]);
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound == false){
+                    txtView.setText("Nema pronajdeni prevodi --- No Translations found");
+                }
+            }
+        });
     }
 }
